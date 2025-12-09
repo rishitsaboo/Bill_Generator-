f9302d0b1569ce9c3745bc74ab9eed4cd2e6429b
rishitsaboo <saboorishit27@gmail.com>
2025-12-09 18:46:20 +0530

    commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import BillGenerator from "./pages/bill_preview";
+
+function App() {
+  return (
+    <div className="h-scree w-screen bg-gray-100">
+      <BillGenerator />
+    </div>
+  )
+}
+export default App;
